c86ad26a5d241a0853b6e334acbeea54b2d91a6b
榊原昌彦 <sakakibara@rdlabo.jp>
2018-07-29 18:33:21 +0900

    add presentToast.

--- pages/index.vue
+++ pages/index.vue
@@ -11,6 +11,26 @@
     <ion-content>
       The world is your oyster.
       <p>If you get lost, the <a target="_blank" href="https://ionicframework.com/docs">docs</a> will be your guide.</p>
+
+      <ion-button @click="presentToast">start Toast</ion-button>
     </ion-content>
+    <ion-toast-controller></ion-toast-controller>
   </ion-page>
 </template>
+
+<script>
+  export default {
+    methods: {
+      async presentToast() {
+        const toastController = document.querySelector('ion-toast-controller');
+        await toastController.componentOnReady();
+
+        const toast = await toastController.create({
+          message: 'Your settings have been saved.',
+          duration: 200000
+        });
+        return await toast.present();
+      }
+    }
+  }
+</script>
